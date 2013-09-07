7ba0ccb3cc26a6bb53f55bfcd31189144bbc09a4
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-09-07 07:36:13 +0000

    m

--- web.js
+++ web.js
@@ -1,67 +1,40 @@
-#!/usr/bin/env node
-var  express = require('express')
-  , fs = require('fs')
-  , http = require('http')
-  , https = require('https')
-var counter=0;
+var express = require("express"),
+    http = require("http");
+
 var app = express();
-app.set('port', process.env.PORT || 8080);
-app.configure(function(){
-app.use(express.bodyParser());
-app.use(app.router);
-
-});
-
-http.createServer(app).listen(app.get('port'), function() {
-    counter+=1;
-      console.log("Listening on " + app.get('port'));
-  });
-
-socket=require("socket.io");
-io=socket.listen(app);
+var server = http.createServer(app);
+var io = require("socket.io").listen(server);
 
 io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
 });
 
-// routing
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + "/index.html");
 });
 
-// usernames which are currently connected to the chat
-var usernames = {};
+io.sockets.on("connection", function (socket) {
+    socket.emit("from server", { message: "Welcome to Jeff's Chat Room!" });
+    sendAll({online: Object.keys(socket.manager.open).length});
+    socket.on("from client", function (data) {
+    console.log("received: ", data, " from ", socket.store.id);
 
-io.sockets.on('connection', function (socket) {
-
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.emit('updatechat', socket.username, data);
-	});
-
-	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
-		// we store the username in the socket session for this client
-		socket.username = username;
-		// add the client's username to the global list
-		usernames[username] = username;
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected');
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-		// update the list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
-	});
-
-	// when the user disconnects.. perform this
-	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-	});
+    if (data.message)
+        sendAll(data, socket.id);
+    });
+    
+    socket.on("disconnect", function(reason) {
+        sendAll({online: Object.keys(socket.manager.open).length});
+    });
 });
+
+function sendAll(message, user) {
+    for (var socket in io.sockets.sockets) {
+        if (socket != user)
+            io.sockets.sockets[socket].emit("from server", message);
+    }
+}
